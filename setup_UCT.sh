b6595ee82fc5876c930bd29ce52db248b09a59d7
Tiana Schwab <tiana.schwab@gmail.com>
2018-10-18 16:57:01 +0200

    some extras

---
+++ setup_UCT.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Setting path to Vienna Programs"
+export PATH="$PATH:/home/tiana/IBFunSS/Programs/ViennaRNA/Progs/VRP/bin/"
+
---
+++ setup_home.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Setting path to Vienna Programs"
+export PATH="$PATH:/home/schwab/ViennaRNA/bin/"
+echo "Setting path to RaxML"
+export PATH="$PATH:/home/schwab/standard-RAxML-master/"
+
+
