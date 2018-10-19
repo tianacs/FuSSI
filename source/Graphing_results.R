8ed04ca4f2fc1425feaaaf2c71609fee1cda0f15
Tiana Schwab <tiana.schwab@gmail.com>
2018-10-19 15:58:16 +0200

    19 Oct

--- source/Graphing_results.R
+++ source/Graphing_results.R
@@ -5,6 +5,6 @@ getwd ()
 # set working directory to location of your Graph_<run_ID>.csv file
 setwd("/home/tiana/FuSSI/source")
 
-rl <- read.csv ("Graph_extracts_test.csv")
+rl <- read.csv ("Graph_s5UTR.csv")
 library (ggplot2)
-ggplot(data=rl, aes(x=rl$Position, y=log(rl$Relative_Likelihood), group = 1))+geom_line(size=1.0)+labs(x="Position in MSA", y="log(Relative Likelihood)")+scale_x_continuous(breaks=seq(0,10000,500))
+ggplot(data=rl, aes(x=rl$Position, y=log(rl$Relative_Likelihood), group = 1))+geom_line(size=1.0)+labs(x="Position in MSA", y="log(Relative Likelihood)")+scale_x_continuous(breaks=seq(0,500, 50))
