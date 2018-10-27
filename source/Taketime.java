06a217b88c8e715eb7917af5606fea3e96fd10d1
Tiana Schwab <tiana.schwab@gmail.com>
2018-10-27 21:39:38 +0200

    27 Oct

--- source/Taketime.java
+++ source/Taketime.java
@@ -8,9 +8,11 @@ public class Taketime {
 	public static void main (String[] args_) {
 	
 		// Test MSA of sequence length 200, 500 (y), 1000, 5000 (y), 10000 (y)
-		String[] files = {"HIV_171_5000.fas"};
+		//String[] files = {"HIV_171_5000.fas"};
+		String[] files = {"HIV_70_500.fas", "HIV_100_500.fas","HCV_247_500.fas"};
 		
 		for (int i = 0; i < files.length; i++) {
+			System.out.println ("Running "+files[i]);
 			long startTime = System.currentTimeMillis ();
 			try {
 				Runtime rt = Runtime.getRuntime();
@@ -23,7 +25,6 @@ public class Taketime {
 			}
 			long stopTime = System.currentTimeMillis ();
 			long elapsedTime = stopTime- startTime;
-			System.out.println ("Run: "+files[i]);
 			System.out.println (elapsedTime+" Milliseconds");
 			System.out.println (elapsedTime/1000.0+" Seconds");
 			System.out.println (elapsedTime/1000.0/60+" Minutes");
