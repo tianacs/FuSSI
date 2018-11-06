7de67c4e3636ed390ed5af38c74cf55b9a6b6d9f
Tiana Schwab <tiana.schwab@gmail.com>
2018-11-06 13:19:26 +0200

    06 Nov

--- Graphing_results.R
+++ Graphing_results.R
@@ -3,22 +3,14 @@ rm (list=ls())    #clear workspace
 getwd ()
 
 # set working directory to location of your Graph_<run_ID>.csv file
-setwd("/home/tiana/FuSSI/HCV_Sequences/Output")
-setwd ("C:/Users/Tiana/Desktop/Project/FuSSI/HCV_Sequences/Output")
+setwd("/home/...")
 
-rl <- read.csv ("Graph_Mauger250rerun.csv")
+rl <- read.csv ("Graph_<run_ID.csv")
 library (ggplot2)
 ggplot(data=rl, aes(x=rl$Position, y=log(rl$Relative_Likelihood), group = 1))+
 labs(x="Position in MSA", y="log(Relative Likelihood)")+
   scale_x_continuous(breaks=seq(0,10000,500))+
+  #highlight a region in your alignment using:
   geom_rect (data=NULL, aes (xmin = 101, xmax = 343, ymin=-Inf, ymax=Inf), colour = "grey",fill = "lightgreen", alpha=0.02)+
-  geom_rect (data=NULL, aes (xmin = 9511, xmax = 9583, ymin=-Inf, ymax=Inf), colour = "grey", fill = "lightgreen", alpha=0.02)+
-  geom_rect (data=NULL, aes (xmin = 9744, xmax = 9791, ymin=-Inf, ymax=Inf), colour = "grey", fill = "lightgreen", alpha=0.02)+
-  geom_rect (data=NULL, aes (xmin = 765, xmax = 840, ymin=-Inf, ymax=Inf), colour = "grey", fill = "lightgreen", alpha=0.02)+
-  geom_rect (data=NULL, aes (xmin = 8228, xmax = 8398, ymin=-Inf, ymax=Inf), colour = "grey",fill = "lightgreen", alpha=0.02)+
-  geom_rect (data=NULL, aes (xmin = 9047, xmax = 9136, ymin=-Inf, ymax=Inf), colour = "grey",fill = "lightgreen", alpha=0.02)+
-  geom_rect (data=NULL, aes (xmin = 9293, xmax = 9470, ymin=-Inf, ymax=Inf), colour = "grey",fill = "lightgreen", alpha=0.02)+
-  geom_rect (data=NULL, aes (xmin = 402, xmax = 437, ymin=-Inf, ymax=Inf), colour = "grey",fill = "lightgreen", alpha=0.02)+
-  geom_rect (data=NULL, aes (xmin = 441, xmax = 521, ymin=-Inf, ymax=Inf),colour = "grey", fill = "lightgreen", alpha=0.02)+
   geom_line(size=1.0)
 
